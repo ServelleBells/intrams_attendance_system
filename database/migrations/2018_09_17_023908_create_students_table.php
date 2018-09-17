5305edf55b238760510ed8251e46a5f18b625bca
0515-3685-cect@CEC.EDU.PH <0515-3685-cect@CEC.EDU.PH>
2018-09-17 10:58:40 +0800

    Student model:

--- database/migrations/2018_09_17_023908_create_students_table.php
+++ database/migrations/2018_09_17_023908_create_students_table.php
@@ -15,7 +15,7 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('id#')->unique();
+            $table->string('student_id')->unique();
             $table->string('last_name');
             $table->string('first_name');
             $table->string('course');
