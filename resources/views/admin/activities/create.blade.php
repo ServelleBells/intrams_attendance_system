42999472941266373c67452bfd3cd7370d2bbfac
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-09-24 10:48:47 +0800

    fix error

--- resources/views/admin/activities/create.blade.php
+++ resources/views/admin/activities/create.blade.php
@@ -1,12 +1,12 @@
 <center>
 	<br>
-<form action="{{ $action }}" method="POST">
+<form action="" method="POST">
 							{{ csrf_field() }}
 							<label>New Activity</label><br/>
 							<label>Name:</label><br/>
-							<input type="text" name="name" value="{{$activities->name}}"/>
+							<input type="text" name="name" value=""/>
 							<br><br>							
-							<button type="submit"><a href="{{ route('activities.id.show', array('id'=>$activities->id)) }}">Show</a</button>
+							<button type="submit"><a href="">Show</a</button>
 							<br/>
 						</form>
 
