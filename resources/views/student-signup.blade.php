f367d100e29ed18fcc6bb75cc4a4e400d48a0b1e
janicamae <pauljohntadem24@gmail.com>
2018-09-24 11:20:07 +0800

    Student Signup

--- resources/views/student-signup.blade.php
+++ resources/views/student-signup.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Sign-up</title>
+        <title>Student Sign-up</title>
 		<style>
 			html, body {
                 background-color: #fff;
