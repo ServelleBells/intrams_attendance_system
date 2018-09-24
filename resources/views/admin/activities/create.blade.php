a4ec0f4de22ba2661823492357c52b34e8df8dac
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-09-24 10:50:01 +0800

    fix activities view

--- resources/views/admin/activities/create.blade.php
+++ resources/views/admin/activities/create.blade.php
@@ -1,3 +1,8 @@
+
+@extends('layouts.app')
+
+@section('content')
+
 <center>
 	<br>
 <form action="" method="POST">
@@ -6,8 +11,10 @@
 							<label>Name:</label><br/>
 							<input type="text" name="name" value=""/>
 							<br><br>							
-							<button type="submit"><a href="">Show</a</button>
+							<button type="submit"><a href="">Create</a</button>
 							<br/>
 						</form>
 
 </center>						
+
+@endsection
