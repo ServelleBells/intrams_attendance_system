4a99dad5265f3a5784023f7c1dc74b7973bd91d5
Jhon Ace Casabuena <Casabuenaace1997@gmail.com>
2018-10-03 10:41:58 +0800

    userstable seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,25 +11,24 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('users')->insert([
-            'name' => 'admin',
-            'account_type_id' => 1,
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin123'),
-        ]);
-
-         DB::table('users')->insert([
-            'name' => 'treasurer',
-            'account_type_id' => 2,
-            'email' => 'treasurer@gmail.com',
-            'password' => bcrypt('treasurer123'),
-        ]);
-
-         DB::table('users')->insert([
-            'name' => 'checker',
-            'account_type_id' => 3,
-            'email' => 'checker@gmail.com',
-            'password' => bcrypt('checker123'),
-        ]);
+         DB::table('users')->insert(
+		  [ 
+		    [
+		      'account_types_id'=>1,
+		      'name'=> 'admin',
+		      'email' => 'admin@gmail.com',
+		      'passsword' => bcrypt('admin123'),
+		    ],[
+		      'account_types_id'=>2,
+		      'name'=> 'checker',
+		      'email' => 'checker@gmail.com',
+		      'passsword' => bcrypt('checker123'),
+		    ],[
+		      'account_types_id'=>3,
+		      'name'=> 'treasurer',
+		      'email' => 'treasurer@gmail.com',
+		      'passsword' => bcrypt('treasurer123'),
+		    ]
+		  ]);
     }
 }
