8235e31dd1bed08352fa157c215ae7edb3131884
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-10-08 11:37:59 +0800

    main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,24 +71,28 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <!--<a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>-->
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Cristal-E
+                </div>
+
+                <div>
+                    Intrams Attendance System
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="{{ route('login') }}">Login to start</a>
+                    <!--<a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>-->
                 </div>
             </div>
         </div>
